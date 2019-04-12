73012d78610717ab85d2738e3a69fc0e10e8b446
Coloradosunrae <stevie@ourhopecenter.org>
2019-04-11 18:16:36 -0600

    rename folders

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
     friends
   };
 
-  removeFriend = id => {
+  clickAmount = id => {
     // Filter this.state.friends for friends with an id not equal to the id being removed
     const friends = this.state.friends.filter(friend => friend.id !== id);
     // Set this.state.friends equal to the new friends array
@@ -36,13 +36,11 @@ class App extends Component {
         <Title>Friends List</Title>
         {this.state.friends.map(friend => (
           <FriendCard
-            removeFriend={this.removeFriend}
+            clickAmount={this.clickAmount}
             id={friend.id}
             key={friend.id}
             name={friend.name}
             image={friend.image}
-            occupation={friend.occupation}
-            location={friend.location}
           />
         ))}
       </Wrapper>
